e99fa32d9c227b51fbe7c2657bfe8b20eba56d9c
chandu89 <chandanwtb2009@gmail.com>
2024-11-18 12:04:56 +0100

    Added rubicon for code styling

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
+# The StringCalculator class is responsible for performing
+# calculations on strings containing numbers.
+# It supports operations such as adding numbers from a
+# comma-separated string and handling custom delimiters.
+#
+# Example usage:
+#   calculator = StringCalculator.new
+#   result = calculator.add("1,2,3") # => 6
+#   result = calculator.add("//;\n1;2") # => 3
+#
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
   describe '.add' do
     it 'returns 0 for an empty string' do
-      expect(StringCalculator.add("")).to eq(0)
+      expect(StringCalculator.add('')).to eq(0)
     end
 
     it 'returns the number itself for a single number' do
-      expect(StringCalculator.add("1")).to eq(1)
+      expect(StringCalculator.add('1')).to eq(1)
     end
 
     it 'returns the sum of two numbers' do
-      expect(StringCalculator.add("1,2")).to eq(3)
+      expect(StringCalculator.add('1,2')).to eq(3)
     end
   end
 end
