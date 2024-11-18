4b241d8688c991efb24c4271693d5cfaeeea517f
chandu89 <chandanwtb2009@gmail.com>
2024-11-18 12:23:20 +0100

    Added custom delimiters failed test(Rspec)

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -26,6 +26,10 @@ RSpec.describe StringCalculator do
       it 'should handles newline as delimiters' do
         expect(StringCalculator.add('1\n2,3')).to eq(6)
       end
+
+      it 'should supports custom delimiters' do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      end
     end
   end
 end
