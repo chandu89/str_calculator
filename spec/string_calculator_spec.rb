d825ff6e3c76e55631acbc978c199cfbda89987b
chandu89 <chandanwtb2009@gmail.com>
2024-11-18 12:10:34 +0100

    Spec optimised with context and added failed newline check

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,20 +4,28 @@ require 'string_calculator'
 
 RSpec.describe StringCalculator do
   describe '.add' do
-    it 'returns 0 for an empty string' do
-      expect(StringCalculator.add('')).to eq(0)
+    context 'when without delimiters' do
+      it 'returns 0 for an empty string' do
+        expect(StringCalculator.add('')).to eq(0)
+      end
+
+      it 'returns the number itself for a single number' do
+        expect(StringCalculator.add('1')).to eq(1)
+      end
+
+      it 'returns the sum of two numbers' do
+        expect(StringCalculator.add('1,2')).to eq(3)
+      end
+
+      it 'returns the sum of any amount of numbers' do
+        expect(StringCalculator.add('1,2,3,4,5')).to eq(15)
+      end
     end
 
-    it 'returns the number itself for a single number' do
-      expect(StringCalculator.add('1')).to eq(1)
-    end
-
-    it 'returns the sum of two numbers' do
-      expect(StringCalculator.add('1,2')).to eq(3)
-    end
-
-    it 'returns the sum of any amount of numbers' do
-      expect(StringCalculator.add('1,2,3,4,5')).to eq(15)
+    context 'when with delimiters' do
+      it 'should handles newline as delimiters' do
+        expect(StringCalculator.add('1\n2,3')).to eq(6)
+      end
     end
   end
 end
